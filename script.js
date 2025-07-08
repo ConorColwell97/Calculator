c73e3397c7f507a226f5fb17de6d706974403570
ConorColwell97 <conormcolwell@gmail.com>
2025-07-08 22:45:21 +0100

    Implemented full functionality for calculator
    Can now use decimal numbers, decimals round to
    one place, and can now go back on pressed buttons

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const input = document.querySelector('#input');
 const output = document.createElement('p');
 input.appendChild(output);
 let expression;
+let dotUsed = false;
 
 for(let i = 0; i <= 9; i++) {
     const button = document.createElement('button');
@@ -16,10 +17,26 @@ for(let i = 0; i <= 9; i++) {
         } else {
             expression += button.textContent;
         }
+
+        dotUsed = false;
         output.innerText = expression;
     });
 }
 
+const dot = document.createElement('button');
+dot.textContent = '.';
+dot.classList.add('digit');
+digit_container.appendChild(dot);
+
+dot.addEventListener('click', () => {
+    if(!dotUsed) {
+        expression += '.';
+        output.innerText = expression;
+        dotUsed = true;
+    }
+});
+
+    
 const sign_container = document.querySelector('#sign_container');
 const signs = ['+', '-', 'x', '/'];
 
@@ -30,7 +47,13 @@ signs.forEach((sign) => {
     sign_container.appendChild(button);
 
     button.addEventListener('click', () => {
-        expression += (' ' + button.textContent + ' ');
+        if(expression === undefined || expression === null) {
+            expression = (' ' + button.textContent + ' ');
+        } else {
+            expression += (' ' + button.textContent + ' ');
+        }
+
+        dotUsed = false;
         output.innerText = expression;
     });
 });
@@ -42,27 +65,72 @@ sign_container.appendChild(button);
 
 button.addEventListener('click', () => {
     const arr = expression.split(' ');
-    output.innerText = expression + ' = ' + operate(arr[0], arr[1], arr[2]);
+    let result = operate(arr[0], arr[2], arr[1]);
+
+    for(let i = 3; i < arr.length-1; i += 2) {
+        result = operate(result, arr[i+1], arr[i]);
+    }
+
+    if(isNaN(result)) {
+        output.innerText = 'Invalid expression';
+    } else {
+        result = (isInteger(result)) ? parseInt(result) : result.toFixed(1);
+        output.innerText = expression + ' = ' + result;
+    }
+
+    dotUsed = false;
     expression = null;
 });
 
-function operate(a, sign, b) {
-    let result;
+const back = document.createElement('button');
+back.textContent = 'back';
+back.classList.add('back');
+sign_container.appendChild(back);
 
+back.addEventListener('click', () => {
+    expression = expression.slice(0, expression.length-1);
+    output.innerText = expression;
+});
+
+const clear = document.createElement('button');
+clear.textContent = 'clear';
+clear.classList.add('clear');
+sign_container.appendChild(clear);
+
+clear.addEventListener('click', () => {
+    output.innerHTML = null;
+    expression = null;
+    dotUsed = false;
+    curr = null;
+});
+
+function operate(a, b, sign) {
+    if(isNaN(a) || isNaN(b)) {
+        return 'Invalid';
+    }
+
+    let result;
     switch(sign) {
         case '+':
-            result = add(parseInt(a), parseInt(b));
+            result = add(parseFloat(a), parseFloat(b));
             break;
         case '-':
-            result = subtract(parseInt(a), parseInt(b));
+            result = subtract(parseFloat(a), parseFloat(b));
             break;
         case 'x':
-            result = multiply(parseInt(a), parseInt(b));
+            result = multiply(parseFloat(a), parseFloat(b));
             break;
         case '/':
-            result = divide(parseInt(a), parseInt(b));
+            result = divide(parseFloat(a), parseFloat(b));
+            if(result == 'Division by 0') {
+                return 'Cannot divide by 0!';
+            }
             break;
         default: alert('Invalid input');
+    }
+
+    if(isNaN(result)) {
+        return 'Invalid expression';
     }
 
     return result;
@@ -83,8 +151,12 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if(b == 0) {
-        return 'Cannot divide by zero';
+        return 'Division by 0';
     }
 
     return a / b;
+}
+
+function isInteger(result) {
+    return result - parseInt(result) === 0; 
 }
